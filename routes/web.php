3cddee98116d6a112eced50fa4bc0f17af3c0755
Kevleoymg <kevin.perez@yellomg.com>
2024-06-13 19:01:17 -0600

    about us

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about-us', function () {
+    return view('about-us');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
