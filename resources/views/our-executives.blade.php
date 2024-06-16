9f51423ac0e782bf87f4253df31be7e24c31372a
jmotta21 <jonathan.motta@yellomg.com>
2024-06-16 12:08:52 -0600

    Our Executives
    Create layout

---
+++ resources/views/our-executives.blade.php
@@ -0,0 +1,193 @@
+@extends('layouts.layout')
+@section('content')
+	<div class="parallax" data-background="images/headOffice.jpeg" data-color="#36383e" data-color-opacity="0.45" data-img-width="2500" data-img-height="1600">
+		<div class="parallax-content">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="main-search-container">
+							<h2>Our Executives</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="main-search-input margin-bottom-40">
+				</div>
+			</div>
+			<div class="col-md-12">
+				<div class="row">
+					<div class="About Us-grid-container">
+						<div class="grid-item col-lg-3 col-md-4 col-sm-6 col-xs-12">
+							<div class="agent">
+								<div class="agent-avatar">
+									<a href="#">
+										<img src="images/executive/WhatsApp Image 2024-03-16 at 8.49.37 PM.jpeg"  alt="">
+									</a>
+								</div>
+								<div class="agent-content">
+									<div class="agent-name">
+										<h4><a href="#">Mr. Ian Gill</a></h4>
+										<span>General Manager</span>
+									</div>
+									<ul class="agent-contact-details">
+										<li><i class="sl sl-icon-call-in"></i>(123) 123-456</li>
+										<li><i class="fa fa-envelope-o "></i><a href="#">tom@example.com</a></li>
+									</ul>
+									<ul class="social-icons">
+										<li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+										<li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+										<li><a class="gplus" href="#"><i class="icon-gplus"></i></a></li>
+										<li><a class="linkedin" href="#"><i class="icon-linkedin"></i></a></li>
+									</ul>
+									<div class="clearfix"></div>
+								</div>
+
+							</div>
+						</div>
+						<div class="grid-item col-lg-3 col-md-4 col-sm-6 col-xs-12">
+							<div class="agent">
+								<div class="agent-avatar">
+									<a href="#">
+										<img src="images/executive/WhatsApp Image 2024-03-16 at 8.49.37 PM (1).jpeg" style="max-width: 218px" alt="">
+									</a>
+								</div>
+								<div class="agent-content">
+									<div class="agent-name">
+										<h4><a href="#">Ms. Fiona Goodridge</a></h4>
+										<span>Deputy General Manager (Preretirement)</span>
+									</div>
+									<ul class="agent-contact-details">
+										<li><i class="sl sl-icon-call-in"></i>(123) 123-456</li>
+										<li><i class="fa fa-envelope-o "></i><a href="#">jennie@example.com</a></li>
+									</ul>
+									<ul class="social-icons">
+										<li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+										<li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+										<li><a class="gplus" href="#"><i class="icon-gplus"></i></a></li>
+										<li><a class="linkedin" href="#"><i class="icon-linkedin"></i></a></li>
+									</ul>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item col-lg-3 col-md-4 col-sm-6 col-xs-12">
+							<div class="agent">
+								<div class="agent-avatar">
+									<a href="#">
+										<img src="images/executive/WhatsApp Image 2024-03-16 at 8.49.37 PM (2).jpeg" style="max-width: 210px" alt="">
+									</a>
+								</div>
+								<div class="agent-content">
+									<div class="agent-name">
+										<h4><a href="#">Mr. Roger Ward</a></h4>
+										<span>Deputy General Manager (ag)</span>
+									</div>
+									<ul class="agent-contact-details">
+										<li><i class="sl sl-icon-call-in"></i>(123) 123-456</li>
+										<li><i class="fa fa-envelope-o "></i><a href="#">david@example.com</a></li>
+									</ul>
+									<ul class="social-icons">
+										<li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+										<li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+										<li><a class="gplus" href="#"><i class="icon-gplus"></i></a></li>
+										<li><a class="linkedin" href="#"><i class="icon-linkedin"></i></a></li>
+									</ul>
+									<div class="clearfix"></div>
+								</div>
+
+							</div>
+						</div>
+						<div class="grid-item col-lg-3 col-md-4 col-sm-6 col-xs-12">
+							<div class="agent">
+								<div class="agent-avatar">
+									<a href="#">
+										<img src="images/executive/WhatsApp Image 2024-03-16 at 8.49.37 PM (3).jpeg" style="max-width: 195px" alt="">
+									</a>
+								</div>
+								<div class="agent-content">
+									<div class="agent-name">
+										<h4><a href="#">Mr. Raymond Lorde</a></h4>
+										<span>Chief Technical Officer (ag)</span>
+									</div>
+									<ul class="agent-contact-details">
+										<li><i class="sl sl-icon-call-in"></i>(123) 123-456</li>
+										<li><i class="fa fa-envelope-o "></i><a href="#">charles@example.com</a></li>
+									</ul>
+									<ul class="social-icons">
+										<li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+										<li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+										<li><a class="gplus" href="#"><i class="icon-gplus"></i></a></li>
+										<li><a class="linkedin" href="#"><i class="icon-linkedin"></i></a></li>
+									</ul>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="About Us-grid-container">
+						<div class="grid-item col-lg-3 col-md-4 col-sm-6 col-xs-12">
+							<div class="agent">
+								<div class="agent-avatar">
+									<a href="#">
+										<img src="images/executive/WhatsApp Image 2024-03-16 at 8.49.37 PM (4).jpeg" alt="">
+									</a>
+								</div>
+
+								<div class="agent-content">
+									<div class="agent-name">
+										<h4><a href="#">Mrs Henrietta Bourne-Forde</a></h4>
+										<span>Chief Legal Officer</span>
+									</div>
+									<ul class="agent-contact-details">
+										<li><i class="sl sl-icon-call-in"></i>(123) 123-456</li>
+										<li><i class="fa fa-envelope-o "></i><a href="#">tom@example.com</a></li>
+									</ul>
+									<ul class="social-icons">
+										<li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+										<li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+										<li><a class="gplus" href="#"><i class="icon-gplus"></i></a></li>
+										<li><a class="linkedin" href="#"><i class="icon-linkedin"></i></a></li>
+									</ul>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item col-lg-3 col-md-4 col-sm-6 col-xs-12">
+							<div class="agent">
+								<div class="agent-avatar">
+									<a href="#">
+										<img src="images/executive/WhatsApp Image 2024-03-16 at 8.49.38 PM.jpeg" alt="">
+									</a>
+								</div>
+								<div class="agent-content">
+									<div class="agent-name">
+										<h4><a href="#">Ms Kaye Hinds</a></h4>
+										<span>Board Secretary</span>
+									</div>
+									<ul class="agent-contact-details">
+										<li><i class="sl sl-icon-call-in"></i>(123) 123-456</li>
+										<li><i class="fa fa-envelope-o "></i><a href="#">jennie@example.com</a></li>
+									</ul>
+									<ul class="social-icons">
+										<li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+										<li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+										<li><a class="gplus" href="#"><i class="icon-gplus"></i></a></li>
+										<li><a class="linkedin" href="#"><i class="icon-linkedin"></i></a></li>
+									</ul>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
