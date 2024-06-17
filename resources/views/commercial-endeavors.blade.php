c052c6ecfb41b4cb966278d4fddd071e4261cf43
jmotta21 <jonathan.motta@yellomg.com>
2024-06-17 15:57:38 -0600

    Update commercial-endeavors.blade.php
    add banner botton

--- resources/views/commercial-endeavors.blade.php
+++ resources/views/commercial-endeavors.blade.php
@@ -1,95 +1,131 @@
 @extends('layouts.layout')
 @section('content')
-	<div class="parallax" data-background="images/headOffice.jpeg" data-color="#36383e" data-color-opacity="0.45" data-img-width="2500" data-img-height="1600">
-		<div class="parallax-content">
+    <div class="parallax" data-background="images/headOffice.jpeg" data-color="#36383e" data-color-opacity="0.45"
+        data-img-width="2500" data-img-height="1600">
+        <div class="parallax-content">
 
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
 
-						<!-- Main Search Container -->
-						<div class="main-search-container">
-							<h2>Commercial Endeavors</h2>
-
-
-						</div>
-						<!-- Main Search Container / End -->
-
-					</div>
-				</div>
-			</div>
-
-		</div>
-	</div>
-	<div class="container">
-		<div class="row">
-	<div class="col-lg-12 col-md-7 sp-content" style="padding:0px !important">
-		<div class="property-description">
-			<!-- Description -->
-			<br>
-			<br>
-			<div class="input">
-				<p>Our diverse business activities encompass every aspect of the housing sector. From the development of lands, construction of houses, and providing legal and project management services, to offering comprehensive property management solutions, we are a trusted and all-encompassing partner in the realm of real estate. With a rich history and a commitment to excellence, the National Housing Corporation is your go-to resource for all your housing and real estate needs.
-				</p>
-
-				<!-- <a href="#" class="show-more-button">Show More <i class="fa fa-angle-down"></i></a> -->
-			</div>
-		</div>
-	</div>
-		</div>
-	</div>
-	<div class="container">
-		<div class="row">
+                        <!-- Main Search Container -->
+                        <div class="main-search-container">
+                            <h2>Commercial Endeavors</h2>
 
 
+                        </div>
+                        <!-- Main Search Container / End -->
 
-				<div class="toggle-wrap style-2  "><span class="trigger "><a href="#"> Real Estate Development Of Lands
-<i class="sl sl-icon-plus"></i></a></span><div class="toggle-container"><p>We specialize in the strategic development of land properties, transforming raw land into valuable real estate assets. Our expertise in land development ensures that each project is thoughtfully designed and executed to create thriving communities and investment opportunities.
+                    </div>
+                </div>
+            </div>
 
-						</p>
-					</div></div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-7 sp-content" style="padding:0px !important">
+                <div class="property-description">
+                    <!-- Description -->
+                    <br>
+                    <br>
+                    <div class="input">
+                        <p>Our diverse business activities encompass every aspect of the housing sector. From the
+                            development of lands, construction of houses, and providing legal and project management
+                            services, to offering comprehensive property management solutions, we are a trusted and
+                            all-encompassing partner in the realm of real estate. With a rich history and a commitment to
+                            excellence, the National Housing Corporation is your go-to resource for all your housing and
+                            real estate needs.
+                        </p>
 
-				<div class="toggle-wrap style-2  "><span class="trigger "><a href="#"> Construction Of Houses
-<i class="sl sl-icon-plus"></i></a></span><div class="toggle-container"><p>Our team of experienced professionals is dedicated to bringing your dream home to life. We offer comprehensive house construction services, ensuring that each project is built with precision and attention to detail, providing you with a comfortable and stylish living space.
+                        <!-- <a href="#" class="show-more-button">Show More <i class="fa fa-angle-down"></i></a> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
 
 
 
-						</p>
-					</div>
-					<div class="toggle-wrap style-2  "><span class="trigger "><a href="#"> Legal Services
+            <div class="toggle-wrap style-2  "><span class="trigger "><a href="#"> Real Estate Development Of Lands
+                        <i class="sl sl-icon-plus"></i></a></span>
+                <div class="toggle-container">
+                    <p>We specialize in the strategic development of land properties, transforming raw land into valuable
+                        real estate assets. Our expertise in land development ensures that each project is thoughtfully
+                        designed and executed to create thriving communities and investment opportunities.
 
-<i class="sl sl-icon-plus"></i></a></span><div class="toggle-container"><p>We specialize in the strategic development of land properties, transforming raw land into valuable real estate assets. Our expertise in land development ensures that each project is thoughtfully designed and executed to create thriving communities and investment opportunities.
+                    </p>
+                </div>
+            </div>
 
-
-							</p>
-						</div>
-						<div class="toggle-wrap style-2  "><span class="trigger "><a href="#"> Project Management
-
-
-<i class="sl sl-icon-plus"></i></a></span><div class="toggle-container"><p>With a focus on efficiency and excellence, our project management services ensure that your real estate projects are completed on time and within budget. We oversee every aspect of the project, from planning to execution, to deliver successful outcomes.
-								</p>
-							</div>
-							<div class="toggle-wrap style-2  "><span class="trigger "><a href="#"> Property Management
-
-
-
-<i class="sl sl-icon-plus"></i></a></span><div class="toggle-container"><p>For property owners, our property management services offer peace of mind and convenience. We take care of the day-to-day operations, maintenance, and tenant relations for your real estate investments, ensuring they are well-maintained and generate consistent returns.
+            <div class="toggle-wrap style-2  "><span class="trigger "><a href="#"> Construction Of Houses
+                        <i class="sl sl-icon-plus"></i></a></span>
+                <div class="toggle-container">
+                    <p>Our team of experienced professionals is dedicated to bringing your dream home to life. We offer
+                        comprehensive house construction services, ensuring that each project is built with precision and
+                        attention to detail, providing you with a comfortable and stylish living space.
 
 
-									</p>
-								</div>
-							</div>
-							<br>
-							<br>
-							<br>
 
-							<!-- Property Description -->
+                    </p>
+                </div>
+                <div class="toggle-wrap style-2  "><span class="trigger "><a href="#"> Legal Services
 
-							<!-- Property Description / End -->
-						</div>
-					</div>
-				</div>
+                            <i class="sl sl-icon-plus"></i></a></span>
+                    <div class="toggle-container">
+                        <p>We specialize in the strategic development of land properties, transforming raw land into
+                            valuable real estate assets. Our expertise in land development ensures that each project is
+                            thoughtfully designed and executed to create thriving communities and investment opportunities.
 
-		</div>
-	</div>
+
+                        </p>
+                    </div>
+                    <div class="toggle-wrap style-2  "><span class="trigger "><a href="#"> Project Management
+
+
+                                <i class="sl sl-icon-plus"></i></a></span>
+                        <div class="toggle-container">
+                            <p>With a focus on efficiency and excellence, our project management services ensure that your
+                                real estate projects are completed on time and within budget. We oversee every aspect of the
+                                project, from planning to execution, to deliver successful outcomes.
+                            </p>
+                        </div>
+                        <div class="toggle-wrap style-2  "><span class="trigger "><a href="#"> Property Management
+
+
+
+                                    <i class="sl sl-icon-plus"></i></a></span>
+                            <div class="toggle-container">
+                                <p>For property owners, our property management services offer peace of mind and
+                                    convenience. We take care of the day-to-day operations, maintenance, and tenant
+                                    relations for your real estate investments, ensuring they are well-maintained and
+                                    generate consistent returns.
+
+
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+                        <br>
+
+                        <!-- Property Description -->
+
+                        <!-- Property Description / End -->
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <a href="/contact" class="flip-banner parallax" data-background="images/back.jpg" data-color="#"
+    data-color-opacity="0.1" data-img-width="2500" data-img-height="1600">
+    <div class="flip-banner-content">
+        <h2 class="flip-visible">Not Sure Of Your Next Step</h2>
+        <h2 class="flip-hidden">Get In Contact Today <i class="sl sl-icon-arrow-right"></i></h2>
+    </div>
+</a>
 @endsection
