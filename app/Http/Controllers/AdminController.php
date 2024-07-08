a24474547e46748d67afd33f7748eb993a42dd16
jmotta21 <jonathan.motta@yellomg.com>
2024-07-08 11:25:51 -0600

    Review Check Exist About us

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Blade;
+
+use App\Models\AboutUs;
+
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $recordAboutsUs = AboutUs::where('id', 1)->exists();
+
+        return view('admin.index', compact('recordAboutsUs'));
         //return " All users are shown here";
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,13 +5,23 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="containerOut">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg">
+            <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg containerInner">
 
-                <a href="{{ url('/admin/about-us/edit/1') }}" class="btn btn-xs btn-info pull-right">
-                    About Us, Vision and Mission
-                </a>
+                <!--Record about us-->
+                @if ($recordAboutsUs)
+                    <a href="{{ url('/admin/about-us/edit/1') }}" class="btn btn-xs btn-info pull-right text-white">
+                        About Us, Vision and Mission
+                    </a>
+                @else
+                    <a href="{{ url('/admin/about-us/edit/1') }}" class="btn btn-xs btn-info pull-right text-white">
+                        Create About Us, Vision and Mission Content
+                    </a>
+                @endif
+                <!--Record about us-->
+
+
             </div>
         </div>
     </div>
