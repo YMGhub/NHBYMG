66ee3e0e0c1e00e930881d4cf25ec7310e2d21a5
jmotta21 <jonathan.motta@yellomg.com>
2024-08-05 14:33:47 -0600

    update image and css name

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -16,7 +16,7 @@
 
 
 
-    <link rel="stylesheet" href="{{ asset('css/style_new23.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style_new24.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style-template.css') }}">
     <link rel="stylesheet" href="{{ asset('css/color.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fancybox.css') }}">
