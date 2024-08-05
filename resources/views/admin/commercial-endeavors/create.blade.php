c425f53f751943c17256ad0f043036ec7ce57022
jmotta21 <jonathan.motta@yellomg.com>
2024-08-05 14:56:57 -0600

    Update create.blade.php

--- resources/views/admin/commercial-endeavors/create.blade.php
+++ resources/views/admin/commercial-endeavors/create.blade.php
@@ -40,7 +40,7 @@
                         <div>
                             <label for=""
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                            <input type="text" id="question" name="question"
+                            <input type="text" id="name_commercial" name="name_commercial"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="" required />
                         </div>
@@ -49,7 +49,7 @@
                         <div>
                             <label for=""
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                            <textarea id="answer" name="answer"
+                            <textarea id="description_commercial" name="description_commercial"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="" required /></textarea>
                         </div>
