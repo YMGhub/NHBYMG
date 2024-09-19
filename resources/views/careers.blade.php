60197fb22f92c57d53e416070c9fa08a64a05421
jmotta21 <jonathan.motta@yellomg.com>
2024-09-19 16:51:53 -0600

    Update careers.blade.php
    add description

--- resources/views/careers.blade.php
+++ resources/views/careers.blade.php
@@ -10,12 +10,15 @@
             vertical-align: middle;
             margin-right: 5px;
         }
-        .radio-label input[type="radio"] {
-            width: 15px; /* Set width */
-            height: 15px; /* Set height */
-            margin-right: 5px; /* Adjust spacing between radio button and label */
-        }
 
+        .radio-label input[type="radio"] {
+            width: 15px;
+            /* Set width */
+            height: 15px;
+            /* Set height */
+            margin-right: 5px;
+            /* Adjust spacing between radio button and label */
+        }
     </style>
 
     <!-- Map Container -->
@@ -42,20 +45,31 @@
                 <section id="contact">
                     <h4 class="headline margin-bottom-35">Careers Form</h4>
 
+                    <p>
+                        At the National Housing Corporation, we are always on the lookout for passionate, skilled
+                        professionals to join our team. We offer diverse opportunities to contribute to our mission of
+                        providing high-quality housing solutions.<br>
+                        If you're ready to make a difference and grow your career in a rewarding environment, we encourage
+                        you to apply today. Together, we can build a brighter future for communities across the nation.
+                    </p>
+
                     <div id="contact-message"></div>
 
-                    <form method="post" action="{{ route('careers.apply') }}" name="careerApplicationForm" id="careerApplicationForm" enctype="multipart/form-data" autocomplete="on">
+                    <form method="post" action="{{ route('careers.apply') }}" name="careerApplicationForm"
+                        id="careerApplicationForm" enctype="multipart/form-data" autocomplete="on">
                         @csrf
                         <div class="row">
                             <div class="col-md-6">
                                 <div>
-                                    <input name="first_name" type="text" id="first_name" placeholder="First Name" required="required" />
+                                    <input name="first_name" type="text" id="first_name" placeholder="First Name"
+                                        required="required" />
                                 </div>
                             </div>
 
                             <div class="col-md-6">
                                 <div>
-                                    <input name="last_name" type="text" id="last_name" placeholder="Last Name" required="required" />
+                                    <input name="last_name" type="text" id="last_name" placeholder="Last Name"
+                                        required="required" />
                                 </div>
                             </div>
                         </div>
@@ -78,13 +92,15 @@
 
                             <div class="col-md-6">
                                 <div>
-                                    <input name="age" type="number" id="age" placeholder="Age" required="required" />
+                                    <input name="age" type="number" id="age" placeholder="Age"
+                                        required="required" />
                                 </div>
                             </div>
                         </div>
 
                         <div>
-                            <input name="phone_home" type="tel" id="phone_home" placeholder="Phone (Home)" required="required" />
+                            <input name="phone_home" type="tel" id="phone_home" placeholder="Phone (Home)"
+                                required="required" />
                         </div>
 
                         <div>
@@ -92,21 +108,25 @@
                         </div>
 
                         <div>
-                            <input name="phone_mobile" type="tel" id="phone_mobile" placeholder="Phone (Mobile)" required="required" />
+                            <input name="phone_mobile" type="tel" id="phone_mobile" placeholder="Phone (Mobile)"
+                                required="required" />
                         </div>
 
                         <div>
-                            <input name="email" type="email" id="email" placeholder="Email Address" required="required" />
+                            <input name="email" type="email" id="email" placeholder="Email Address"
+                                required="required" />
                         </div>
 
                         <div><label for="resume">Upload Resume/CV (PDF, DOC, DOCX, TXT)</label>
-                            <input name="resume" type="file" id="resume" accept=".pdf,.doc,.docx,.txt" required="required">
+                            <input name="resume" type="file" id="resume" accept=".pdf,.doc,.docx,.txt"
+                                required="required">
 
                         </div>
 
                         <div>
                             <label for="cover_letter">Upload Cover Letter (PDF, DOC, DOCX, TXT)</label>
-                            <input name="cover_letter" type="file" id="cover_letter" accept=".pdf,.doc,.docx,.txt" required="required">
+                            <input name="cover_letter" type="file" id="cover_letter" accept=".pdf,.doc,.docx,.txt"
+                                required="required">
 
                         </div>
 
@@ -124,8 +144,8 @@
     <!-- Container / End -->
 @endsection
 <script>
-    document.addEventListener('DOMContentLoaded', function () {
-        document.getElementById('careerApplicationForm').addEventListener('submit', function (event) {
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('careerApplicationForm').addEventListener('submit', function(event) {
             event.preventDefault(); // Prevent the form from submitting normally
 
             // Assuming you have included SweetAlert library script tag
@@ -134,33 +154,33 @@
             let formData = new FormData(this);
 
             fetch(this.action, {
-                method: this.method,
-                body: formData
-            })
+                    method: this.method,
+                    body: formData
+                })
                 .then(response => {
-                if (!response.ok) {
-                    Swal.fire({
-                title: 'Error!',
-                text: 'An error occurred while submitting the form. Please try again later.',
-                icon: 'error',
-                confirmButtonText: 'OK'
-            });
-                    throw new Error('Network response was not ok');
-                }else{
+                    if (!response.ok) {
+                        Swal.fire({
+                            title: 'Error!',
+                            text: 'An error occurred while submitting the form. Please try again later.',
+                            icon: 'error',
+                            confirmButtonText: 'OK'
+                        });
+                        throw new Error('Network response was not ok');
+                    } else {
 
-                    Swal.fire({
-                        title: 'Success!',
-                        text: "Submitted successfully", // Assuming the server returns a message key in the JSON response
-                        icon: 'success',
-                        confirmButtonText: 'OK'
-                    });
+                        Swal.fire({
+                            title: 'Success!',
+                            text: "Submitted successfully", // Assuming the server returns a message key in the JSON response
+                            icon: 'success',
+                            confirmButtonText: 'OK'
+                        });
 
 
-                }
-            return response.json();
-        })
+                    }
+                    return response.json();
+                })
 
-        document.getElementById('careerApplicationForm').reset();
+            document.getElementById('careerApplicationForm').reset();
 
         });
     });
