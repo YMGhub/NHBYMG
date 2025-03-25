2c475abe87af5bb718c20e451b14a30c23abe67b
jmotta21 <jonathan.motta@yellomg.com>
2025-03-25 16:12:20 -0600

    Add type form type Application Call
    Rental 2
    Purchase 3

--- app/Http/Controllers/ApiValidationController.php
+++ app/Http/Controllers/ApiValidationController.php
@@ -14,13 +14,24 @@ class ApiValidationController extends Controller {
         if (!$apiSetting || !$apiSetting->api_url || !$apiSetting->auth_key || !$apiSetting->auth_value) {
             return response()->json(['error' => 'Configuración de API no encontrada.'], 400);
         }
+        
+        
+
+        //ApplicationType 2=rental and 3 purchase
+        $typeApplication = 2;
+
+        if($request->type_form == "Rental"){
+            $typeApplication = 2;
+        }else{
+            $typeApplication = 3;
+        }
 
         try {
             $response = Http::withHeaders([
                 $apiSetting->auth_key => $apiSetting->auth_value
             ])->post($apiSetting->api_url, [
                 'NRN' => $request->number_national,
-                'Applicationtype' => 2
+                'Applicationtype' => $typeApplication
             ]);
 
             return response()->json($response->json());
